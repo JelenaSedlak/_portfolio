6506fb3c67a9d976bbf5edc1e17269408b95886d
JelenaSedlak <sedlakjelena.24@gmail.com>
2020-08-13 14:59:03 +0200

    my project

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav_link');
+
+
+navToggle.addEventListener('click', () => {
+	document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		document.body.classList.remove('nav-open')
+	})
+});
+
+
+mybutton = document.getElementById("btn_up");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
